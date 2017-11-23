2d5a3937b82b41a62e64451685e0dfba0858a4eb
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2017-11-23 20:44:59 +0000

    Rudimentary interpolation script

--- adaptivity.py
+++ adaptivity.py
@@ -1,6 +1,7 @@
 from firedrake import *
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def intervalAdapt(mesh, f, tolCoarsen, tolRefine):
@@ -11,8 +12,10 @@ def intervalAdapt(mesh, f, tolCoarsen, tolRefine):
     :param tolRefine: tolerance above which mesh is to be refined.
     :return: adapted mesh.
     """
+    # TODO: only currently works for P1 fields
+
     x1 = mesh.coordinates.dat.data      # Get mesh coordinates
-    d = x1[-1] - x1[0]                   # Get length of interval
+    d = x1[-1] - x1[0]                  # Get length of interval
     # x1.append(x1[0])                    # Append coordinate list for periodic boundary
     x2 = []                             # List for new coordinate set
     fData = f.dat.data
@@ -33,15 +36,32 @@ def intervalAdapt(mesh, f, tolCoarsen, tolRefine):
 
 # TODO: above does not work for PeriodicIntervalMesh, as the .dat.data structure is different
 
-# def interpolateVariable(mesh, f):
-#     # TODO
+def interpolateVariable(mesh, f):
+    """
+    :param mesh: new mesh to interpolate onto.
+    :param f: function to interpolate.
+    :return: function f interpolated onto new mesh.
+    """
+    V = f.function_space()
+    family = V.ufl_element().family()
+    try:
+        assert family in ('Lagrange', 'Discontinuous Lagrage')
+    except:
+        raise NotImplementedError('Function space type %s not yet implemented' % family)
+    g = Function(FunctionSpace(mesh, family, V.ufl_element().degree()))
+    for i in range(len(mesh.coordinates.dat.data)):
+        g.dat.data[i] = f.at(mesh.coordinates.dat.data[i])
+    return g
+
 
 if __name__ == '__main__':
     mesh = IntervalMesh(8, 1)
-    print('Mesh coords pre-adapt: ', mesh.coordinates.dat.data)
     X = SpatialCoordinate(mesh)
     f = Function(FunctionSpace(mesh, 'CG', 1)).interpolate(X**2)
+    plt.plot(mesh.coordinates.dat.data, f.dat.data)
+    plt.show()
 
     mesh2 = intervalAdapt(mesh, f, 0.1, 0.5)
-    print('Mesh coords post-adapt: ', mesh2.coordinates.dat.data)
-
+    f2 = interpolateVariable(mesh2, f)
+    plt.plot(mesh2.coordinates.dat.data, f2.dat.data)
+    plt.show()
